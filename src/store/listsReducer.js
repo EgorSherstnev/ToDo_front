499ccb22ba460f1733551a8359073b56ce75a346
EgorSherstnev <svarog91@list.ru>
2023-08-04 01:20:26 +0300

    add reducer

--- src/store/listsReducer.js
+++ src/store/listsReducer.js
@@ -1,3 +1,16 @@
-const defaultState = {
+import { 
+   SET_LISTS 
+} from "../actions/types";
+
+const initialState = {
    lists: []
+}
+
+export default function listsReducer(state = initialState, action) {
+   switch (action.type) {
+      case SET_LISTS:
+         return {...state, lists:action.payload };
+      default:
+         return state;
+   }
 }
